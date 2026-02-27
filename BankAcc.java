9f99be658f20f0412e3aebd02e95f84dbcc79514
Arun-velmurugan <arunrajvel02@gmail.com>
2026-02-27 10:25:06 +0530

    Java workout

---
+++ BankAcc.java
@@ -0,0 +1,14 @@
+class BankAcc
+{
+	public static void main(String[] args)
+	{
+		long accNo = 12345678901l;
+		String accHolderName = "Anitha";
+		float accBal = 25000.5f;
+		String accType = "Savings";
+		System.out.println(accNo);
+		System.out.println(accHolderName);
+		System.out.println(accBal);
+		System.out.println(accType);
+	}
+}
