98b4a91e461b62856ebefc4d5afb0ae769faa985
Arun-velmurugan <arunrajvel02@gmail.com>
2026-02-28 13:32:18 +0530

    Homework

---
+++ ArithOP.java
@@ -0,0 +1,30 @@
+class ArithOp
+{
+	public static void main(String[] args)
+	{
+		int a = 18;
+		int b = 49;
+		//Addition
+		int c = a+b;
+		//Subtraction
+		int d = c-b;
+		//Multiply
+		int e = a*2;
+		//Divide
+		int f = a/6;
+		//Modulus
+		int g = a%6;
+		//Increment 
+		int h = g++;
+		//Decrement
+		int i = h--;
+		System.out.println(c);
+	    System.out.println(d);
+	    System.out.println(e);
+        System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+
+	}
+}
---
+++ RelationalOp.java
@@ -0,0 +1,26 @@
+class RelationalOp
+{
+	public static void main(String[] args)
+	{
+		int a = 18;
+		int b = 10;
+		//Equalto
+		boolean c = a==b; //false
+		//NotEqualto
+		boolean d = a!=b; //true
+		//Greaterthan
+		boolean e = b>a;  //false
+		//Lessthan
+		boolean f = a<19;  //true
+		//Greaterthan or Equalto
+		boolean g = b>=11; //false
+		//Lessthan or Equalto
+		boolean h = a<=18; //true
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+	}
+}
