ff87744122a44bb29b2b83f3aa8d8bcb39bf926a
Arun-velmurugan <arunrajvel02@gmail.com>
2026-03-03 21:32:08 +0530

    java workout

---
+++ TicketPrice.java
@@ -0,0 +1,19 @@
+class TicketPrice
+{
+	public static void main(String[] args)
+	{
+		int age = 69;
+		if (age<12)
+		{
+			System.out.println("Ticket Price:80");
+		}
+		else if (age>=12 && age<60)
+		{
+			System.out.println("Ticket Price:150");
+		}
+		else
+		{
+			System.out.println("Ticket Price:100");
+		}
+	}
+}
---
+++ TrafficSign.java
@@ -0,0 +1,20 @@
+class TrafficSign
+{
+	public static void main(String[] args)
+	{
+		char signal = 'G';//Also try R,Y
+		
+		if (signal=='R')
+		{
+			System.out.println("STOP");
+		}
+		if (signal=='Y')
+		{
+			System.out.println("READY");
+		}
+		if (signal=='G')
+		{
+			System.out.println("GO");
+		}
+	}
+}
